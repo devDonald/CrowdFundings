c16e0b44e56f1e245051397024264e8caf62ace1
devDonald <donaldebuga@gmail.com>
2018-05-22 16:58:02 +0100

    resolve uid issues

--- app/src/main/java/com/donald/crowdfunding/activities/MainActivity.java
+++ app/src/main/java/com/donald/crowdfunding/activities/MainActivity.java
@@ -3,9 +3,8 @@ package com.donald.crowdfunding.activities;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
-import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
-import android.support.design.widget.Snackbar;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -14,9 +13,9 @@ import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 
 import com.donald.crowdfunding.business.R;
+import com.donald.crowdfunding.fragments.Profile;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
@@ -55,14 +54,7 @@ public class MainActivity extends AppCompatActivity
             }
         };
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
@@ -126,7 +118,14 @@ public class MainActivity extends AppCompatActivity
         }else if (id==R.id.recommend){
 
         }else if (id==R.id.nav_profile){
-
+            Bundle bundle = new Bundle();
+            bundle.putString("userId", uid);
+            //bundle.putString("userEmail", authUserEmail);
+            FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+            Profile profileActivity = new Profile();
+            profileActivity.setArguments(bundle);
+            fragmentTransaction.replace(R.id.content_main, profileActivity);
+            fragmentTransaction.commit();
 
         }
         else if (id == R.id.nav_editProfile) {
